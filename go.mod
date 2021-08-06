f3db05b3f449dbe45267eeb3020bf4ce1115587e
Keerthan Ekbote <keerthan.ekbote@solo.io>
2021-08-06 12:00:12 -0400

    add main.go

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.16
 
 require (
 	github.com/envoyproxy/go-control-plane v0.9.9-0.20210511190911-87d352569d55
-	github.com/solo-io/go-utils v0.21.14
-	github.com/solo-io/solo-kit v0.21.1 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.4.0 // indirect
+	github.com/google/go-cmp v0.5.1 // indirect
+	golang.org/x/mod v0.3.1-0.20200828183125-ce943fd02449 // indirect
 	golang.org/x/tools v0.0.0-20200825202427-b303f430e36d
 )
--- main.go
+++ main.go
@@ -89,20 +89,16 @@ func reportResult(e errcheck.Result) {
 	if err != nil {
 		wd = ""
 	}
-	for _, uncheckedError := range e.UncheckedErrors {
-		pos := uncheckedError.Pos.String()
+	for _, unusedGetterError := range e.UnusedGetterError {
+		pos := unusedGetterError.Pos.String()
 		if !abspath {
 			newPos, err := filepath.Rel(wd, pos)
 			if err == nil {
 				pos = newPos
 			}
 		}
-
-		if verbose && uncheckedError.FuncName != "" {
-			fmt.Printf("%s:\t%s\t%s\n", pos, uncheckedError.FuncName, uncheckedError.Line)
-		} else {
-			fmt.Printf("%s:\t%s\n", pos, uncheckedError.Line)
-		}
+		// Print result to stdout
+		fmt.Printf("%s:\t%s\t%s\n", pos, unusedGetterError.FuncName, unusedGetterError.Line)
 	}
 }
 
@@ -118,7 +114,7 @@ func mainCmd(args []string) int {
 	if rc != exitCodeOk {
 		return rc
 	}
-
+	// Check paths
 	result, err := checkPaths(&checker, paths...)
 	if err != nil {
 		if err == errcheck.ErrNoGoFiles {
@@ -128,7 +124,8 @@ func mainCmd(args []string) int {
 		fmt.Fprintf(os.Stderr, "error: failed to check packages: %s\n", err)
 		return exitFatalError
 	}
-	if len(result.UncheckedErrors) > 0 {
+	// Report unused getter error if errors are found
+	if len(result.UnusedGetterError) > 0 {
 		reportResult(result)
 		return exitUncheckedError
 	}
@@ -177,26 +174,15 @@ func parseFlags(checker *errcheck.Checker, args []string) ([]string, int) {
 
 	var checkAsserts, checkBlanks bool
 
-	flags.BoolVar(&checkBlanks, "blank", false, "if true, check for errors assigned to blank identifier")
-	flags.BoolVar(&checkAsserts, "asserts", false, "if true, check for ignored type assertion results")
 	flags.BoolVar(&checker.Exclusions.TestFiles, "ignoretests", false, "if true, checking of _test.go files is disabled")
 	flags.BoolVar(&checker.Exclusions.GeneratedFiles, "ignoregenerated", false, "if true, checking of files with generated code is disabled")
 	flags.BoolVar(&verbose, "verbose", false, "produce more verbose logging")
-
 	flags.BoolVar(&abspath, "abspath", false, "print absolute paths to files")
 
-	tags := tagsFlag{}
-	flags.Var(&tags, "tags", "comma or space-separated list of build tags to include")
 	ignorePkg := flags.String("ignorepkg", "", "comma-separated list of package paths to ignore")
 	ignore := ignoreFlag(map[string]*regexp.Regexp{})
 	flags.Var(ignore, "ignore", "[deprecated] comma-separated list of pairs of the form pkg:regex\n"+
 		"            the regex is used to ignore names within pkg.")
-
-	var excludeFile string
-	flags.StringVar(&excludeFile, "exclude", "", "Path to a file containing a list of functions to exclude from checking")
-
-	var excludeOnly bool
-	flags.BoolVar(&excludeOnly, "excludeonly", false, "Use only excludes from -exclude file")
 
 	flags.StringVar(&checker.Mod, "mod", "", "module download mode to use: readonly or vendor. See 'go help modules' for more.")
 
@@ -207,20 +193,6 @@ func parseFlags(checker *errcheck.Checker, args []string) ([]string, int) {
 	checker.Exclusions.BlankAssignments = !checkBlanks
 	checker.Exclusions.TypeAssertions = !checkAsserts
 
-	if !excludeOnly {
-		checker.Exclusions.Symbols = append(checker.Exclusions.Symbols, errcheck.DefaultExcludedSymbols...)
-	}
-
-	if excludeFile != "" {
-		excludes, err := errcheck.ReadExcludes(excludeFile)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Could not read exclude file: %v\n", err)
-			return nil, exitFatalError
-		}
-		checker.Exclusions.Symbols = append(checker.Exclusions.Symbols, excludes...)
-	}
-
-	checker.Tags = tags
 	for _, pkg := range strings.Split(*ignorePkg, ",") {
 		if pkg != "" {
 			checker.Exclusions.Packages = append(checker.Exclusions.Packages, pkg)
